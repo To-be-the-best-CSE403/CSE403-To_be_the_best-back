0aafb1a31aa3a99748c5addbd881607e6aa72c77
TsaiGai <iantsai11267@gmail.com>
2024-01-30 14:17:43 -0800

    Added extraction folder

---
+++ backend/src/extraction/basicqueries.sql
@@ -0,0 +1,14 @@
+/*
+Basic queries will consist of accessing a pokemon's type, ability, tier, popular moves,
+and EVs/IVs. 
+*/
+
+-- accessing the type
+
+-- accessing the ability
+
+-- accessing the tier
+
+-- accessing the popular moves
+
+-- accessing the EVs/IVs
