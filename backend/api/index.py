b1c305aea11a46ab27e811f9686b25b18bdd1df4
PopoDev <67870588+PopoDev@users.noreply.github.com>
2024-01-31 22:04:23 -0800

    Fix API CORS Error with Flask (#31)

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from api.views import views
 from api.test_views import test_views
 
 app = Flask(__name__)
+CORS(app)
 
 # Registering production endpoints
 app.register_blueprint(views, url_prefix='/api')
