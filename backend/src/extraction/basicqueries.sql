265d77ea0c7ea78731ae8e2463941a2b04a2dc02
TsaiGai <iantsai11267@gmail.com>
2024-02-01 14:34:35 -0800

    Updated basicqueries.sql

--- backend/src/extraction/basicqueries.sql
+++ backend/src/extraction/basicqueries.sql
@@ -1,14 +1,23 @@
 /*
-Basic queries will consist of accessing a pokemon's type, ability, tier, popular moves,
-and EVs/IVs. 
+Basic queries will consist of accessing a pokemon's type, ability, popular moves, and EVs/IVs. 
 */
 
 -- accessing the type
+/* Queries under this category assume that there are two tables: one consisting of the names
+of each species of pokemon and one contains the types of that species.*/
+SELECT types.species
+FROM pokebase base, poketypes types
+WHERE base.species = types.species
+GROUP BY types.species;
 
 -- accessing the ability
-
--- accessing the tier
+/* Queries under this category assume that there are two tables: one consisting of the names
+of each species of pokemon and one contains the types of that species.*/
 
 -- accessing the popular moves
+/* Queries under this category assume that there are two tables: one consisting of the names
+of each species of pokemon and one contains the types of that species.*/
 
 -- accessing the EVs/IVs
+/* Queries under this category assume that there are two tables: one consisting of the names
+of each species of pokemon and one contains the types of that species.*/
