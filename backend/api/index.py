b6b67202c4deaaab3c780a87b18e84b063beea84
PopoDev <67870588+PopoDev@users.noreply.github.com>
2024-01-31 17:21:41 -0800

    Add Mock API For Teambuilder (#29)

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,5 +1,12 @@
 from flask import Flask
 from api.views import views
+from api.test_views import test_views
 
 app = Flask(__name__)
+
+# Registering production endpoints
 app.register_blueprint(views, url_prefix='/api')
+
+# Registering test endpoints
+app.register_blueprint(test_views, url_prefix='/api/test')
+
