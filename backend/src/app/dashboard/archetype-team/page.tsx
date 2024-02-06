4a84d595767db01b51f812b8ce4a750b0454bd1e
Joselyn Do <joselynd@uw.edu>
2024-02-06 14:19:33 -0800

    Adding new files for type effectivenss method and new webpage

---
+++ backend/src/app/dashboard/archetype-team/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import ArchtypeTeamMdx from "./archetype-team.mdx";
+
+export default function ArchtypeTeam() {
+  return (
+    <div>
+      <ArchtypeTeamMdx />
+    </div>
+  );
+}
