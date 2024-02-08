aad438219c85f1b8e60b9cd6b959f3df76d3edb7
PopoDev <popocompte@gmail.com>
2024-02-07 17:20:06 -0800

    Add API Teambuilder with Archetype

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,64 +1,46 @@
 from flask import Blueprint, request, jsonify
+from src.api.teambuilder.teambuilder_api import get_team
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
 
 
-@views.route('/home')
+@views.route("/home")
 def api_home():
-    return 'Hello, World!'
+    return "Hello, World!"
 
 
-@views.route('/teambuilder')
+@views.route("/teambuilder", methods=["GET"])
 def api_teambuilder():
-    return [
-        {
-            'name': 'Pikachu',
-            'item': 'Light Ball',
-            'ability': 'Static',
-            'nature': 'Timid',
-            'moves': [
-                'Thunderbolt',
-                'Volt Switch',
-                'Grass Knot',
-                'Quick Attack',
-            ],
-        },
-        {
-            'name': 'Charizard',
-            'item': 'Assault Vest',
-            'ability': 'Blaze',
-            'nature': 'Modest',
-            'moves': [
-                'Flamethrower',
-                'Air Slash',
-                'Dragon Pulse',
-                'Focus Blast',
-            ],
-        }
-    ]
+    archetype = request.args.get("archetype")
+    if not archetype:
+        return jsonify({"error": "Teambuilder archetype parameter is missing"}), 400
+
+    return get_team(archetype)
 
 
-@views.route('/usage-rate')
+@views.route("/usage-rate")
 def api_usage_rate():
     return [
         {
-            'name': 'Pikachu',
-            'usage': 0.5,
+            "name": "Pikachu",
+            "usage": 0.5,
         },
         {
-            'name': 'Charizard',
-            'usage': 0.5,
+            "name": "Charizard",
+            "usage": 0.5,
         },
     ]
 
 
-@views.route('/common-movesets', methods=['GET'])
+@views.route("/common-movesets", methods=["GET"])
 def api_common_movesets():
-    pokemon = request.args.get('pokemon')
+    pokemon = request.args.get("pokemon")
 
     if not pokemon:
-        return jsonify({'error': 'Missing "pokemon" parameter'}), 400
-    
-    return jsonify({
-        'pokemon': pokemon,
-    })
+        return jsonify({"error": 'Missing "pokemon" parameter'}), 400
+
+    return jsonify(
+        {
+            "pokemon": pokemon,
+        }
+    )
