1ec13a17b912d662e7b84f4877b5bbc11ffc095c
Joselyn Do <joselynd@uw.edu>
2024-02-08 16:05:51 -0800

    Set up usage rates webpage

--- backend/src/app/api/usage-rate/page.tsx
+++ backend/src/app/api/usage-rate/page.tsx
@@ -2,11 +2,24 @@
 
 import React from "react";
 import UsageRateMdx from "./usage-rate.mdx";
+import BarChart from '@mantine/charts';
+import data from './temp_data';
 
 export default function UsageRate() {
   return (
     <div>
       <UsageRateMdx />
+        <BarChart
+            h={300}
+            data={data}
+            dataKey="pokemon"
+            series={[
+                { name: 'Usage rate (%)', color: 'violet.6' }
+                // { name: 'Laptops', color: 'blue.6' },
+                // { name: 'Tablets', color: 'teal.6' },
+            ]}
+            tickLine="y"
+        />
     </div>
   );
 }
