7b83fae6d07d31ee6d87b5b2b6803663e57af870
Fahad Rai <azed786@cs.washington.edu>
2024-02-12 21:08:49 -0800

    did most of the backend for the move selector, just need to figure out how to deal with variable numbers in the url for a battle

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, jsonify
 from src.api.teambuilder.teambuilder_api import get_team
+from database.webscraper import get_data_specific
+from src.api.movesuggestion.moves_api import compute_effectiveness
 
 views = Blueprint("views", __name__)
 
@@ -16,6 +18,49 @@ def api_teambuilder():
         return jsonify({"error": "Teambuilder archetype parameter is missing"}), 400
 
     return get_team(archetype)
+
+#TODO: might need to merge with above
+@views.route("/teambuilder", methods=["GET"])
+def api_data_selector():
+    name = request.args.get("name")
+    info_type = request.args.get("info_type")
+    if not name and not info_type:
+        return jsonify({"error": "Name and info type parameters are missing for info selector"}), 400
+    if not name:
+        return jsonify({"error": "Name parameter is missing for info selector"}), 400
+    if not info_type:
+        return jsonify({"error": "Info type parameter is missing for info selector"}), 400
+
+    return get_data_specific(name, info_type)
+
+#TODO: figure out how to properly handle the variable number url
+@views.route("/battle-gen9ou-2058129877", methods=["GET"])
+def move_selector():
+    move1type = request.args.get("move1type")
+    move2type = request.args.get("move2type")
+    move3type = request.args.get("move3type")
+    move4type = request.args.get("move4type")
+    
+    move1power = int(request.args.get("move1power"))
+    move2power = int(request.args.get("move2power"))
+    move3power = int(request.args.get("move3power"))
+    move4power = int(request.args.get("move4power"))
+    
+    enemytype1 = request.args.get(enemytype1)
+    enemytype2 = request.args.get(enemytype2)
+    
+    if not move1type or move2type or move3type or move4type:
+        return jsonify(
+            {"error": "A move type parameter is missing for move selector"}), 400
+    
+    if not move1power or move2power or move3power or move4power:
+        return jsonify(
+            {"error": "A move power parameter is missing for move selector"}), 400
+    if not enemytype1:
+        return jsonify(
+            {"error": "Enemey type parameter is missing for move selector"}), 400
+        
+    return compute_effectiveness()
 
 
 @views.route("/usage-rate")
--- backend/src/api/movesuggestion/moves_api.py
+++ backend/src/api/movesuggestion/moves_api.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 # from .moves_algo import opponent_type
 from .moves_algo import most_effective
+import json
 
 TYPES = set()
 
@@ -23,3 +24,59 @@ def get_move(opptype: str):
     optimal = most_effective(opptype)
 
     return jsonify(optimal)
+
+
+def compute_effectiveness(move1type: str, move1power: int,
+                          move2type: str, move2power: int,
+                          move3type: str, move3power: int,
+                          move4type: str, move4power: int,
+                          enemytype1: str, enemytype2: str = ""):
+    """
+    Computes and returns the most effective moves based on the parameters
+    
+    :param move1type: Type of the first move
+    :param move1power: Base power of the first move
+    :param move2type: Type of the second move
+    :param move2power: Base power of the second move
+    :param move3type: Type of the third move
+    :param move3power: Base power of the third move
+    :param move4type: Type of the fourth move
+    :param move4power: Base power of the fourth move
+    :param enemy1type: First type of enemy
+    :param enemy2type: Second type of enemy, (default empty since this type is not guaranteed)
+    
+    :return: The corresponding number for the strongest move
+    """
+    with open("type_chart.json", "r") as json_file:
+        type_chart = json.load(json_file)
+    
+    move1type.lower()
+    move2type.lower()
+    move3type.lower()
+    move4type.lower()
+    
+    enemytype1.lower()
+    enemytype2.lower()
+    
+    powers = [move1power, move2power, move3power, move4power]
+    enemy_types = [enemytype1, enemytype2]
+    move_types = [move1type, move2type, move3type, move4type]
+    
+    max_damage = 0
+    strongest_move = None
+    
+    for move_number, (move_type, move_power) in enumerate(zip(move_types, powers), start=1):
+        if move_power > 0:
+            modifier = 1.0
+            for enemy_type in enemy_types:
+                if enemy_type:
+                    modifier *= type_chart[move_type][enemy_type]
+            
+            damage = move_power * modifier
+            if damage > max_damage:
+                max_damage = damage
+                strongest_move = move_number
+
+        
+    return jsonify({"move_number": strongest_move})
+
