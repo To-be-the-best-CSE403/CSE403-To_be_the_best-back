4cf3673919451b8bb7859e6f577d0b07a923bced
PopoDev <67870588+PopoDev@users.noreply.github.com>
2024-02-15 09:30:12 -0800

    Add Demo Usage Rate Chart (#59)
    * Add Usage Rate Page
    
    * Add Demo Usage Rate Chart

--- backend/src/app/dashboard/usage-rate/page.tsx
+++ backend/src/app/dashboard/usage-rate/page.tsx
@@ -1,41 +1,13 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { UsageRateChart } from "@/components/Chart/UsageRateChart";
-
-interface UsageRateData {
-  pokemon: string;
-  usage: number;
-}
+import { Container } from "@mantine/core";
 
 export default function UsageRate() {
-  /*
-  const [usageRateData, setUsageRateData] = useState<UsageRateData[]>([]);
-  const n = 10;
-
-  useEffect(() => {
-    async () => {
-      try {
-        const response = await fetch(
-          `https://tobethebest.vercel.app/api/usage-rate?n=${n}`
-        );
-        const data = await response.json();
-        const usage = data.map((tuple: any) => {
-          const [_, pokemon, usage] = tuple;
-          return { pokemon, usage };
-        });
-        setUsageRateData(data);
-      } catch (error) {
-        console.error("Error fetching usage rate data:", error);
-      }
-    };
-  }, [n]);
-  */
-
   return (
-    <div>
+    <Container size="lg">
       <h1>Usage Rate</h1>
-      <h2>Top 10 Most Used Pokemon</h2>
-    </div>
+      <h2>Top 10 Most Used Pokemon in January 2024</h2>
+      <img src="/usage-rate.png" alt="Usage.png" />
+    </Container>
   );
 }
