98805cbc6b55a87e2917ad0bef83fb410273e9fd
PopoDev <popocompte@gmail.com>
2024-02-15 09:24:50 -0800

    Add Demo Usage Rate Chart

--- backend/src/app/dashboard/usage-rate/page.tsx
+++ backend/src/app/dashboard/usage-rate/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import UsageRateMdx from "./usage-rate.mdx";
+import { Container } from "@mantine/core";
 
 export default function UsageRate() {
   return (
-    <div>
-      <UsageRateMdx />
-    </div>
+    <Container size="lg">
+      <h1>Usage Rate</h1>
+      <h2>Top 10 Most Used Pokemon in January 2024</h2>
+      <img src="/usage-rate.png" alt="Usage.png" />
+    </Container>
   );
 }
