0665d0145dd8a2fae8e5ebc6fec947a456f40d15
PopoDev <popocompte@gmail.com>
2024-02-15 09:14:48 -0800

    Add Usage Rate Page

--- backend/src/app/dashboard/usage-rate/page.tsx
+++ backend/src/app/dashboard/usage-rate/page.tsx
@@ -1,41 +1,11 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { UsageRateChart } from "@/components/Chart/UsageRateChart";
-
-interface UsageRateData {
-  pokemon: string;
-  usage: number;
-}
+import UsageRateMdx from "./usage-rate.mdx";
 
 export default function UsageRate() {
-  /*
-  const [usageRateData, setUsageRateData] = useState<UsageRateData[]>([]);
-  const n = 10;
-
-  useEffect(() => {
-    async () => {
-      try {
-        const response = await fetch(
-          `https://tobethebest.vercel.app/api/usage-rate?n=${n}`
-        );
-        const data = await response.json();
-        const usage = data.map((tuple: any) => {
-          const [_, pokemon, usage] = tuple;
-          return { pokemon, usage };
-        });
-        setUsageRateData(data);
-      } catch (error) {
-        console.error("Error fetching usage rate data:", error);
-      }
-    };
-  }, [n]);
-  */
-
   return (
     <div>
-      <h1>Usage Rate</h1>
-      <h2>Top 10 Most Used Pokemon</h2>
+      <UsageRateMdx />
     </div>
   );
 }
