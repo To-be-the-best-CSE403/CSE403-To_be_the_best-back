294486d71e29b6159d8b3232860d4de5668b9af4
Joselyn Do <joselynd@uw.edu>
2024-02-22 22:25:05 -0800

    Add files for two about pages

---
+++ backend/src/app/dashboard/about-pages/extension/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import AboutExtensionMdx from "./about-the-extension-page.mdx";
+
+export default function AboutPokemonShowdownPage() {
+    return (
+        <div>
+            <AboutExtensionMdx />
+        </div>
+    );
+}
---
+++ backend/src/app/dashboard/about-pages/pokemon-showdown/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import AboutPokemonShowdownMdx from "./about-pokemon-showdown-page.mdx";
+
+export default function AboutPokemonShowdownPage() {
+    return (
+        <div>
+            <AboutPokemonShowdownMdx />
+        </div>
+    );
+}
