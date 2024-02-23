0e99c1b98aa2aa9f0e0b82eea24bec80aad271af
Joselyn Do <joselynd@uw.edu>
2024-02-22 23:03:42 -0800

    Add an About Pokemon franchise page

---
+++ backend/src/app/dashboard/about-pages/pokemon-franchise/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import AboutPokemonFranchiseMdx from "./about-pokemon-franchise-page.mdx";
+
+export default function AboutPokemonFranchisePage() {
+    return (
+        <div>
+            <AboutPokemonFranchiseMdx />
+        </div>
+    );
+}
