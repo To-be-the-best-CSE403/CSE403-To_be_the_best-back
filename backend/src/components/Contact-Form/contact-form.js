4f32c53f47def6f4c90372bc04df52abf81ae9e5
Joselyn Do <joselynd@uw.edu>
2024-03-01 10:52:02 -0800

    Remove file and add Message label

--- backend/src/components/Contact-Form/contact-form.js
+++ backend/src/components/Contact-Form/contact-form.js
@@ -12,6 +12,7 @@ export default function ContactForm() {
             <label htmlFor="email">Email Address</label>
             <input id="email" type="email" name="email" />
             <ValidationError prefix="Email" field="email" errors={state.errors} />
+            <label htmlFor="message">Message</label>
             <textarea id="message" name="message" />
             <ValidationError prefix="Message" field="message" errors={state.errors} />
             <button type="submit" disabled={state.submitting}>
