f778cfd05edbc37493301feb14b9e08a0f410728
Joselyn Do <joselynd@uw.edu>
2024-03-01 00:18:57 -0800

    Add Contact Us page, must test

---
+++ backend/src/app/dashboard/contact-us/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import ContactUsMdx from "./contact-us.mdx";
+
+export default function ContactUs() {
+    return (
+        <div>
+            <ContactUsMdx />
+        </div>
+    );
+}
