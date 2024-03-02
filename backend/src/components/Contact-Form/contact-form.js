9b653ba84d9074f0c197c1a9c9f977d95c1fce75
Joselyn Do <joselynd@uw.edu>
2024-03-01 22:56:42 -0800

    Try to reform, add div

--- backend/src/components/Contact-Form/contact-form.js
+++ backend/src/components/Contact-Form/contact-form.js
@@ -10,15 +10,21 @@ export default function ContactForm() {
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor="email">Email Address</label>
-            <input id="email" type="email" name="email" size="10"/>
-            <ValidationError prefix="Email" field="email" errors={state.errors} />
-            <label htmlFor="message">Message</label>
-            <textarea id="message" name="message" rows="10" cols="50" />
-            <ValidationError prefix="Message" field="message" errors={state.errors} />
-            <button type="submit" disabled={state.submitting}>
-                Submit
-            </button>
-            <ValidationError errors={state.errors} />
+            <div>
+                <input id="email" type="email" name="email" size="30"/>
+                <ValidationError prefix="Email" field="email" errors={state.errors} />
+            </div>
+            <div>
+                <label htmlFor="message">Message</label>
+                <textarea id="message" name="message" rows="10" cols="50" />
+                <ValidationError prefix="Message" field="message" errors={state.errors} />
+            </div>
+            <div>
+                <button type="submit" disabled={state.submitting}>
+                    Submit
+                </button>
+                <ValidationError errors={state.errors} />
+            </div>
         </form>
     );
 }
