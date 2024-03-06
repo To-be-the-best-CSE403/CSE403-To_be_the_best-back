afb709dab8e23bf0ba66b2a0bfded11826240bf5
PopoDev <popocompte@gmail.com>
2024-03-05 16:22:51 -0800

    Improve Teambuilder.md Layout and Remove Dashboard Page

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,77 +1,92 @@
-import { IconCompass, IconDashboard, IconDeviceAnalytics, IconGauge, IconPokeball, IconInfoCircle, IconVocabulary, IconMail } from '@tabler/icons-react';
-import { NavItem } from '@/types/nav-item';
+import {
+  IconCompass,
+  IconDeviceAnalytics,
+  IconGauge,
+  IconPokeball,
+  IconInfoCircle,
+  IconVocabulary,
+  IconMail,
+} from "@tabler/icons-react";
+import { NavItem } from "@/types/nav-item";
 
 export const navLinks: NavItem[] = [
-	{ label: 'Getting Started', icon: IconCompass, link: '/dashboard/getting-started' },
-	{ label: 'Dashboard', icon: IconDashboard, link: '/dashboard' },
-    {
-        label: 'About Pages',
-        icon: IconInfoCircle,
-        initiallyOpened: true,
-        links: [
-            {
-                label: 'About the extension',
-                link: 'dashboard/about-pages/extension'
-            },
-            {
-                label: 'About Pokémon Showdown',
-                link: 'dashboard/about-pages/pokemon-showdown'
-            },
-            {
-                label: 'About the Pokémon franchise',
-                link: 'dashboard/about-pages/pokemon-franchise'
-            }
-        ]
-    },
-	{
-		label: 'Team Builder',
-		icon: IconPokeball,
-		initiallyOpened: true,
-		links: [
-			{
-				label: 'Overview',
-				link: '/dashboard/teambuilder',
-			},
-			{
-				label: 'Archetype',
-				link: '/dashboard/teambuilder/archetype',
-			},
-			{
-				label: 'API',
-				link: '/dashboard/teambuilder/api',
-			}
-		],
-	},
-	{
-		label: 'Usage Rate',
-		icon: IconGauge,
-		initiallyOpened: true,
-		links: [
-			{
-				label: 'Overview',
-				link: '/dashboard/usage-rate',
-			},
-			{
-				label: 'API',
-				link: '/dashboard/usage-rate/api',
-			}
-		],
-	},
-	{
-		label: 'Common Movesets',
-		icon: IconDeviceAnalytics,
-		initiallyOpened: true,
-		links: [
-			{
-				label: 'Overview',
-				link: '/dashboard/common-movesets',
-			},
-			{
-				label: 'API',
-				link: '/dashboard/common-movesets/api',
-			}
-		],
-	},
-	{ label: 'Pokémon Battle Terminology', icon: IconVocabulary, link: '/dashboard/pokemon-battle-terminology'},
-	{ label: 'Contact Us', icon: IconMail, link: '/dashboard/contact-us'}
+  {
+    label: "Getting Started",
+    icon: IconCompass,
+    link: "/dashboard/getting-started",
+  },
+  {
+    label: "About Pages",
+    icon: IconInfoCircle,
+    initiallyOpened: true,
+    links: [
+      {
+        label: "About the extension",
+        link: "/dashboard/about-pages/extension",
+      },
+      {
+        label: "About Pokémon Showdown",
+        link: "/dashboard/about-pages/pokemon-showdown",
+      },
+      {
+        label: "About the Pokémon franchise",
+        link: "/dashboard/about-pages/pokemon-franchise",
+      },
+    ],
+  },
+  {
+    label: "Team Builder",
+    icon: IconPokeball,
+    initiallyOpened: true,
+    links: [
+      {
+        label: "Overview",
+        link: "/dashboard/teambuilder",
+      },
+      {
+        label: "Archetype",
+        link: "/dashboard/teambuilder/archetype",
+      },
+      {
+        label: "API",
+        link: "/dashboard/teambuilder/api",
+      },
+    ],
+  },
+  {
+    label: "Usage Rate",
+    icon: IconGauge,
+    initiallyOpened: true,
+    links: [
+      {
+        label: "Overview",
+        link: "/dashboard/usage-rate",
+      },
+      {
+        label: "API",
+        link: "/dashboard/usage-rate/api",
+      },
+    ],
+  },
+  {
+    label: "Common Movesets",
+    icon: IconDeviceAnalytics,
+    initiallyOpened: true,
+    links: [
+      {
+        label: "Overview",
+        link: "/dashboard/common-movesets",
+      },
+      {
+        label: "API",
+        link: "/dashboard/common-movesets/api",
+      },
+    ],
+  },
+  {
+    label: "Pokémon Battle Terminology",
+    icon: IconVocabulary,
+    link: "/dashboard/pokemon-battle-terminology",
+  },
+  { label: "Contact Us", icon: IconMail, link: "/dashboard/contact-us" },
 ];
